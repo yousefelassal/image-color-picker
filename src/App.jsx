eaeae0fe2d9b3c59a4416172dc4fd1ec19d5cde7
Yousef <yousefsameh24@gmail.com>
2023-07-05 16:55:19 +0300

    add some shadows

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
 
   return (
     <div className="container">
-      <div className="flex flex-col bg-[#121215] p-4">
+      <div className="flex flex-col bg-[#121215] p-4 border-r border-r-neutral-800 col-shadow">
         <div className="flex gap-1 items-center">
           <img src="/favicon.svg" alt="logo" width="32px"/>
           <h1 className="text-2xl font-bold bg-clip-text text-transparent hover:cursor-default bg-gradient-to-tr from-[#DFE0FB] to-[#8D8FD2]">ColourPicker</h1>
